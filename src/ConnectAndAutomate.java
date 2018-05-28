7d7a2e99d187ae72ca5595a831d011b14492276b
Chand Shubham Bhaskar <Chand.11505573@lpu.in>
2018-05-28 22:12:18 +0530

    Add files via upload

--- src/ConnectAndAutomate.java
+++ src/ConnectAndAutomate.java
@@ -2,26 +2,29 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.util.Iterator;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
 public class ConnectAndAutomate {
 
+	public static WebDriver driver;
+	public static String chromeWebDriverLocation="lib\\chromedriver.exe";
 	//Constants
 	public static final String pageUrl = "https://gtmetrix.com";
 	public static final String userId = "shubhamchand0103@gmail.com";
 	public static final String password = "shubhamchand0103";
-	private static XSSFWorkbook wb;
-	public static WebDriver driver;
-	public static XSSFSheet sh1,sh2,sh3,sh4,sh5;
+
 	//Element selectors
 	public static final String searchBox = "identifier";
 	public static final String analyseButton  = "analyze-form-button";
@@ -32,6 +35,7 @@ public class ConnectAndAutomate {
 	public static final int clicks[] = {1,3,4,5,6};
 	public static final String countryNames[] = {"Dallas, USA","London, UK","Mumbai, India","Sydney, Australia","Sao Paulo, Brazil"};
 	public static String title = "";
+	
 	//Result values
 	public static String pageSpeed;
 	public static String yslow="";
@@ -41,37 +45,72 @@ public class ConnectAndAutomate {
 	public static String pageSpeedIssue;
 	public static String ySlowIssue;
 	public static String checkUrl ;
+	public static String pageSource;
 	
-	static void test(String check, int ind, XSSFSheet sheet, int count){
+	//Excel variables
+	public static XSSFWorkbook wb;
+	public static String inputExcelFileLocation = "Excel\\test.xlsx";
+	public static String outputExcelFileLocation = "Excel\\Result.xlsx";
+	
+	
+	
+	
+	// Extracting result from web page
+	public static void test(String check, int ind, XSSFSheet sheet, int count){
 		
 		driver.get(pageUrl);
 		for(int i=0;i<clicks[count];i++){
 			driver.findElement(By.id(countryButton)).click();
 			for(int k=0;k<50;k++);
 		}
-		
-		String sheetName = driver.findElement(By.id("af-info-region")).getAttribute("innerHTML");
 		driver.findElement(By.xpath("//input[@placeholder='Enter URL to Analyze...']")).sendKeys(check);;
 		driver.findElement(By.xpath("//button[contains(.,'Analyze')]")).click();
+		
 		while(true){
 			title= driver.getTitle();
 			title = title.substring(0, 29);
-			//System.out.println(title);
 			if(title.equals("Latest Performance Report for")){
 				break;
 			}
 		}
-		
-		System.out.println("Result Page : ");
+		/*
+		try {
+	        // Create a URL for the desired page
+			String u = driver.getCurrentUrl();
+	        URL url = new URL(u);       
+
+	        // Read all the text returned by the server
+	        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+	        String str;
+	        while ((str = in.readLine()) != null) {
+	            str = in.readLine().toString();
+	            
+	            System.out.println(str);
+	            // str is one line of text; readLine() strips the newline character(s)
+	        }
+	        in.close();
+	    } catch (MalformedURLException e) {
+	    } catch (IOException e) {
+	    }*/
 		pageSpeed = driver.findElement(By.xpath("//span[@class='report-score-percent']")).getText();
 	    loadTime =  driver.findElement(By.xpath("//*[@class='report-page-detail']/.//span[contains(@class,'report-page-detail-value')]")).getAttribute("innerHTML");
 	    pageSize = driver.findElement(By.xpath("//*[@class='report-page-detail report-page-detail-size']/.//span[contains(@class,'report-page-detail-value')]")).getAttribute("innerHTML");
 	    requests = driver.findElement(By.xpath("//*[@class='report-page-detail report-page-detail-requests']/.//span[contains(@class,'report-page-detail-value')]")).getAttribute("innerHTML");
 	    pageSpeedIssue = driver.findElement(By.xpath("//*[@class='rules-name']/.//a[contains(@href,'#')]")).getAttribute("innerHTML");
 	    driver.findElement(By.xpath("//*[@class='r-tabs-anchor' and @href='#yslow']")).click();
-	    for(int k=0;k<1000;k++);
+	    //delay
+	    for(int k=0;k<20;k++);
 	    ySlowIssue = driver.findElement(By.xpath("//*[@class='rules-name']/.//a[contains(@href,'#')]")).getAttribute("innerHTML");;
 	    
+	    //writing result to excel sheet
+	    
+	    if(Integer.parseInt(pageSpeed)>90){
+	    	pageSpeed = "A"+pageSpeed; 
+	    }else if(Integer.parseInt(pageSpeed)>80){
+	    	pageSpeed = "B" + pageSpeed;
+	    }else{
+	    	pageSpeed = "C" + pageSpeed;
+	    }
 	    Row row = sheet.createRow(ind);
 	    row.createCell(0).setCellValue(check);
 	    row.createCell(1).setCellValue(pageSpeed);
@@ -82,22 +121,25 @@ public class ConnectAndAutomate {
 	    row.createCell(6).setCellValue(pageSpeedIssue);
 	    row.createCell(7).setCellValue(ySlowIssue);
 	    
-	    System.out.println("Load Time: "+loadTime+"\npageSize: "+pageSize+"\nRequests: "+requests+"\nPage Issues: "+pageSpeedIssue+"\nySlow Issues: "+ySlowIssue);
-		
+	    //Printing result to console
+	    System.out.println("Page Speed: "+pageSpeed + "Load Time: "+loadTime+"\npageSize: "+pageSize+"\nRequests: "+requests+"\nPage Issues: "+pageSpeedIssue+"\nySlow Issues: "+ySlowIssue);
+		System.out.println();
 	}
 	
 	public static void main(String[] args) {
 		int count = 0;
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "E:\\#Study materials\\Projects\\Motaingage\\Selenium Jars\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",chromeWebDriverLocation );
 		 driver = new ChromeDriver();
+		 
+		//Logging in to https://www.gtmetrix.com
 		driver.get(pageUrl);
 		driver.findElement(By.className(logInButton)).click();
 		driver.findElement(By.id(userIdBox)).sendKeys(userId);
 		driver.findElement(By.id(passwordBox)).sendKeys(password);
 		driver.findElement(By.xpath("//button[contains(.,'Log In')]")).click();
 		
-		
+		//waiting for the query page
 		while(true){
 			title = driver.getTitle();
 			if(title.equals("Dashboard | GTmetrix")){
@@ -105,27 +147,32 @@ public class ConnectAndAutomate {
 			}
 		}
 		
+		
 		 try{
-			 File src=new File("E:\\eclipse\\Automate\\Excel\\test.xlsx");
+			 //Reading excel file to input the website
+			 File src=new File(inputExcelFileLocation);
 			 FileInputStream fis=new FileInputStream(src);
 			 wb = new XSSFWorkbook(fis);
 			 XSSFSheet  sheet = wb.getSheetAt(0);
 			 wb.setSheetName(0,countryNames[0]);
-			 Iterator<Sheet> sheetIterator = wb.iterator();
-			 while (count<2) {
+			 
+			 //calling test function for 5 countries and all 7 websites
+			 while (count<5) {
 				   if(count>0){
 					   sheet = (XSSFSheet)wb.cloneSheet(0);
 					   wb.setSheetName(count,countryNames[count]);
 				   }
 				   
-			     for(int i=1;i<3;i++){
+			     for(int i=1;i<7;i++){
 						checkUrl = sheet.getRow(i).getCell(0).getStringCellValue();
 						test(checkUrl,i,sheet,count);
 						
 					 }
 			     count++;
 			 }
-			FileOutputStream fout=new FileOutputStream(new File("E:\\eclipse\\Automate\\Excel\\Results.xlsx"));
+			 
+			//Saving the result to new Excel sheet
+			FileOutputStream fout=new FileOutputStream(new File(outputExcelFileLocation));
 			wb.write(fout);
 			fout.close();
 
--- src/ReadWriteExcel.java
+++ src/ReadWriteExcel.java
@@ -1,61 +1,28 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.util.Iterator;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
-public class ReadWriteExcel {
-	private static XSSFWorkbook wb;
-	public static int count = 0;
-	public static String checkUrl = "";
+public class ReadWriteExcel{
 	
-	static void test(String check, int ind, Sheet sheet, int count){
-		
-	
-		
-		
-	    
-	    
-	}
-	
-	
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 try{
-			 File src=new File("E:\\eclipse\\Automate\\Excel\\test.xlsx");
-			 FileInputStream fis=new FileInputStream(src);
-			 wb = new XSSFWorkbook(fis);
-			 
-			 Iterator<Sheet> sheetIterator = wb.iterator();
-			 while (count<5) {
-				 XSSFSheet  sheet = (XSSFSheet)sheetIterator.next();
-			     for(int i=1;i<7;i++){
-						checkUrl = sheet.getRow(i).getCell(0).getStringCellValue();
-						test(checkUrl,i,sheet,count);
-						
-					 }
-			     count++;
-			 }
-		
-			FileOutputStream fout=new FileOutputStream(new File("E:\\eclipse\\Automate\\Excel\\Results.xlsx"));
-			wb.write(fout);
-			fout.close();
+public static void main(String [] args){
+try {
+        // Create a URL for the desired page
+        URL url = new URL("");       
 
-			}catch(Exception e){
-				e.printStackTrace();
-			}
-		
-		
-				
-		System.out.println("Executed Successfully..");
-	}
-
+        // Read all the text returned by the server
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+        String str;
+        while ((str = in.readLine()) != null) {
+            str = in.readLine().toString();
+            System.out.println(str);
+            // str is one line of text; readLine() strips the newline character(s)
+        }
+        in.close();
+    } catch (MalformedURLException e) {
+    } catch (IOException e) {
+    }
+}
 }
